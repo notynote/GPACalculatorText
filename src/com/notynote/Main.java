07aaf0e0a3f42121e6bd44bdc4a8f090dfb294ae
notynote <epochmaking.th@gmail.com>
2021-01-14 15:46:46 +0700

    - update oop

--- src/com/notynote/Main.java
+++ src/com/notynote/Main.java
@@ -22,17 +22,16 @@ public class Main {
             do {
                 try {
                     System.out.println("1. View GPA\n" +
-                            "2. Check eligibility for distinction");
+                            "2. Check eligibility for distinction\n" +
+                            "3. End Program");
                     usermenu = Integer.parseInt(console.next());
                 } catch (Exception ignore){
                     System.out.println("===Error===");
                 }
-            } while (usermenu != 1);
+            } while (usermenu != 1 && usermenu != 2 && usermenu != 3);
             Student student = new Student();
-
             CurrentGPA currentGPA = new CurrentGPA(student);
             Double GPA = currentGPA.getGPA();
-
             int creditLeft = student.requireCredit - student.totalCredit;
             String distinctions = "";
 
@@ -42,15 +41,27 @@ public class Main {
                 } else {
                     distinctions = "You are eligible to earn Distinctions";
                 }
+            } else {
+                distinctions = "You are not eligible for Distinctions";
             }
 
-            System.out.println("Your GPA: " + String.format("%.2f",GPA) +
-                    "\nYou earned credit: " + student.totalCredit +
-                    "\nYou need " + creditLeft + " credits to graduate" +
-                    "\n" + distinctions );
+            if (usermenu == 1) {
+                System.out.println("===================================\n" +
+                        "Your GPA: " + String.format("%.2f",GPA) +
+                        "\nYou earned credit: " + student.totalCredit +
+                        "\nYou need " + creditLeft + " credits to graduate" +
+                        "\n" + distinctions +
+                        "\n===================================\n");
+            } else if (usermenu == 2) {
+                System.out.println("===================================\n" +
+                        distinctions +
+                        "\n===================================\n");
+            } else if (usermenu == 3) {
+                System.out.println("End Program");
+                endprogram = false;
+            }
 
-            System.out.println("End Program");
-            endprogram = false;
+
 
 
         } while (endprogram);
